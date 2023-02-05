4928f7e9a0af203b3fb09f8fdaf9e045f7155278
송민규 <70309556+Alsrbile@users.noreply.github.com>
2023-02-05 19:40:56 +0900

    update

--- MultiLayerPerceptron/src/model.py
+++ MultiLayerPerceptron/src/model.py
@@ -7,12 +7,14 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(512, 512)
         self.fc3 = nn.Linear(512, 10)
         self.act = nn.ReLU()
+        # self.dropout = nn.Dropout(0.3)
+        self.softmax = nn.Softmax(dim=1)
     
     def forward(self, x):
         x = x.reshape(x.shape[0], -1)
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
-        x = self.fc3(x)
+        x = self.softmax(self.fc3(x))
                 
         return x
     
