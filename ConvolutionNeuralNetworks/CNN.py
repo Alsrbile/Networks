4af7a598185ac4be67f5a12815cd411a3a54caac
송민규 <70309556+Alsrbile@users.noreply.github.com>
2023-02-17 13:52:48 +0900

    update

--- ConvolutionNeuralNetworks/CNN.py
+++ ConvolutionNeuralNetworks/CNN.py
@@ -1,25 +1,90 @@
+import torch
+import os
 import argparse
 from torchvision.datasets import FashionMNIST
 import torchvision.transforms as T
 from torch.utils.data import DataLoader 
 from src.model import CNN
-
+import torch.nn as nn
 import torch.optim as optim
+from torchmetrics.functional.classification import accuracy
+from torchmetrics.aggregation import MeanMetric
+from torch.utils.tensorboard import SummaryWriter
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--device",         type=str, default="cuda:0")
-parser.add_argument("--batch_size",     type=int, default=64)
-parser.add_argument("--lr",         type=float, default=0.001)
+parser.add_argument("--device",         type=str,   default="cuda:0")
+parser.add_argument("--batch_size",     type=int,   default=64)
+parser.add_argument("--lr",             type=float, default=0.001)
+parser.add_argument("--epochs",         type=int,   default=50)
+parser.add_argument("--log_dir",        type=str,   default="log")
+parser.add_argument("--checkpoint_dir",     type=str, default="checkpoint")
 args = parser.parse_args()
 
+def train(loader, model, optimizer, scheduler, loss_fn, metric_fn, device):
+    model.train()
+    
+    loss_mean   = MeanMetric().to(device)
+    metric_mean = MeanMetric().to(device)
+    
+    for inputs, targets in loader:
+        inputs  = inputs.to(device)
+        targets = targets.to(device)
+        
+        outputs = model(inputs)
+        loss    = loss_fn(outputs, targets)
+        metric  = metric_fn(outputs, targets)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        loss_mean.update(loss)
+        metric_mean.update(metric)
+        
+        scheduler.step()
+    
+    summary = {"loss": loss_mean.compute(), "metric": metric_mean.compute()}
+    
+    return summary
+
+def evaluate(loader, model, loss_fn, metric_fn, device):
+    # model -> evaluation mode
+    model.eval()
+    
+    # Create average meters to measure loss and accuracy
+    loss_mean   = MeanMetric().to(device)
+    metric_mean = MeanMetric().to(device)
+    
+    # Evaluate mode for one epoch
+    for inputs, targets, in loader:
+        inputs  = inputs.to(device)
+        targets = targets.to(device)
+        
+        # Forward
+        with torch.no_grad():
+            outputs = model(inputs)
+        loss    = loss_fn(outputs, targets)
+        metric  = metric_fn(outputs, targets)
+        
+        # Update statistics
+        loss_mean.update(loss)
+        metric_mean.update(metric)
+    
+    # Summarize statistics
+    summary = {"loss": loss_mean.compute(), "metric": metric_mean.compute()}
+    
+    return summary
 
 def main():
-    print("CNN_Fashion-MNIST")
+    name ="CNN_Fashion-MNIST" 
+    print(name)
+    os.makedirs(f"{args.checkpoint_dir}", exist_ok=True)
     
     # 1. Data processing pipeline (Fashion-MNIST)
     
     transform = T.Compose([
-        T.ToTensor(),
+        T.ToTensor(),       # PIL image -> Tensor
         T.Normalize(0.5, 0.5)
     ])
     
@@ -40,13 +105,58 @@ def main():
     train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=args.batch_size)
     
-    print( train_loader.dataset.data.shape)
+    print(train_loader.dataset.data.shape)
     
     # 2. model
     model = CNN()
     model = model.to(args.device)
     
-    # Training and Evaluation loop
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    # 3. Training and Evaluation loop
+    optimizer   = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler   = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs*len(train_loader))
+    loss_fn     = nn.CrossEntropyLoss()
+    metric_fn   = accuracy
+    
+    train_logger    = SummaryWriter(f"{args.log_dir}/train")
+    test_logger     = SummaryWriter(f"{args.log_dir}/test")
+    
+    if os.path.isfile(f"{args.checkpoint_dir}"):
+        state_dict  = torch.load(f"{args.checkpoint_dir}/{name}_last.pth")
+        start_epoch = state_dict["epoch"]
+        model.load_state_dict(state_dict=["model"])
+        optimizer.load_state_dict(state_dict["optimizer"])
+    else:
+        start_epoch = 0
+    
+    for epoch in range(start_epoch, args.epochs):
+        train_summary   = train(train_loader, model, optimizer, scheduler,
+                                loss_fn, metric_fn, args.device)
+
+        test_summary    = evaluate(test_loader, model, 
+                                loss_fn, metric_fn, args.device)
+        
+        train_logger.add_scalar("Loss", train_summary["loss"], epoch+1)
+        train_logger.add_scalar("Acc", train_summary["metric"], epoch+1)
+        test_logger.add_scalar("Loss", test_summary['loss'], epoch+1)
+        test_logger.add_scalar("Acc", test_summary['metric'], epoch+1)
+        
+        print(f"Epoch {epoch+1}: "
+              + f"Train loss {train_summary['loss']:.04f}, "
+              + f"Train acc {train_summary['metric']:.04f}, "
+              + f"Test loss {test_summary['loss']:.04f}, "
+              + f"Test acc {test_summary['metric']:.04f}")
+        
+        # 4. Save and load subroutine
+        state_dict = {  
+            "epoch":        epoch+1,
+            "model":        model.state_dict(),
+            "optimizer":    optimizer.state_dict(),
+        }
+        
+        torch.save(state_dict, f"{args.checkpoint_dir}/{name}_last.pth")
+    
+    train_logger.close()
+    test_logger.close()
+    
 if __name__ == "__main__":
     main()
--- ConvolutionNeuralNetworks/src/model.py
+++ ConvolutionNeuralNetworks/src/model.py
@@ -1,41 +1,85 @@
 import torch.nn as nn
 
+def conv_block(inputs, in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1):
+    x = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)(inputs)
+    x = nn.BatchNorm2d(out_channels)(x)
+    x = nn.ReLU()(x)
+    x = nn.Dropout()(x) 
+    
+    return x
 
+class CNN_block(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super(CNN_block, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
+        self.drop = nn.Dropout(0.2) 
+    
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.act(x)
+        x = self.drop(x)
+        
+        return x
+        
+        
+    
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         
-     
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1)
-        self.pool1 = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, stride=1)
-        self.pool2 = nn.MaxPool2d(2,2)
+        self.conv_block_11 = CNN_block(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv_block_12 = CNN_block(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv_block_13 = CNN_block(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1)
         
-        self.bn = nn.BatchNorm2d(num_features=28)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout()
+        self.conv_block_21 = CNN_block(in_channels=64, out_channels=96, kernel_size=3, stride=1, padding=1)
+        self.conv_block_22 = CNN_block(in_channels=96, out_channels=96, kernel_size=3, stride=1, padding=1)
+        self.conv_block_23 = CNN_block(in_channels=96, out_channels=96, kernel_size=3, stride=2, padding=1)
         
-        self.fc1 = nn.Linear(64*28*28, 300)
-        self.fc2 = nn.Linear(300, 10)
+        self.conv_block_31 = CNN_block(in_channels=96, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv_block_32 = CNN_block(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv_block_33 = CNN_block(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1)
+        
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=512, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn4 = nn.BatchNorm2d(num_features=512)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        
+        self.fc = nn.Linear(512, 10)
         self.sm = nn.Softmax(dim=1)
         
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+        
+        
+    
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.bn(x)
-        x = self.dropout(x)
-        x = self.pool1(x)
+        # 64, 1, 28, 28
+        x = self.conv_block_11(x)
+        x = self.conv_block_12(x)
+        x = self.conv_block_13(x)
         
-        x = self.conv2(x)
-        x = self.relu(x)
-        x = self.bn(x)
-        x = self.dropout(x)
-        x = self.pool2(x)
+        x = self.conv_block_21(x)
+        x = self.conv_block_22(x)
+        x = self.conv_block_23(x)
         
-        x = x.view(-1, 64*28*28)
-        x = self.fc1(x)
+        x = self.conv_block_31(x)
+        x = self.conv_block_32(x)
+        x = self.conv_block_33(x)
+        # print(x.shape)
+        # 64, 128, 4, 4
+        x = self.conv4(x)
+        # print(x.shape)
+        x = self.bn4(x)
         x = self.relu(x)
-        x = self.fc2(x)
+        x = self.avgpool(x)
+        x = self.dropout(x) 
+        # 64, 32, 26, 26
+        # print(x.shape)
+        
+        x = x.reshape(-1,512)
+        x = self.fc(x)
         x = self.sm(x)
-        
+        # print(x.shape)
         return x
