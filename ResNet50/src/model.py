79a96fb68d77b767034e099c8740257ce48a67db
송민규 <70309556+Alsrbile@users.noreply.github.com>
2023-02-21 13:51:40 +0900

    update

--- ResNet50/src/model.py
+++ ResNet50/src/model.py
@@ -1,5 +1,20 @@
 import torch.nn as nn
 
+def get_resnet(name):
+    
+    model = {"resnet18":ResNet18,
+             "resnet50":ResNet50}
+    
+    if name not in model.keys():
+        raise KeyError(f"{name} is not a valid ResNet version")
+    
+    return model[name]
+
+class ResNet18(nn.Module):
+    def __init__(self):
+        super(self, ResNet18).__init__()
+        
+
 class ResNet50(nn.Module):
     def __init__(self):
         super(self, ResNet50).__init__()
@@ -7,5 +22,7 @@ class ResNet50(nn.Module):
         
     def forward(self, x):
         
+        # 
+                
         return x    
     
