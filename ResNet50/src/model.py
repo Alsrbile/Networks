f1c9556e17c181ec202b4fe804f8801fa617a1d6
송민규 <70309556+Alsrbile@users.noreply.github.com>
2023-02-24 13:40:33 +0900

    update

--- ResNet50/src/model.py
+++ ResNet50/src/model.py
@@ -1,17 +1,34 @@
 import torch.nn as nn
 
 
-
-class CNN_block(nn.Module):
+class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
-        super(CNN_block, self).__init__()
+        super(BasicBlock, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels     = in_channels, 
+            out_channels    = out_channels, 
+            kernel_size     = kernel_size,
+            stride          = stride, 
+            padding         = padding
+        )
+        
+        
+        self.bn = nn.BatchNorm2d(num_features=1)
+        self.gelu = nn.GELU()
+        
 
         
+    def forward(self, input):
+        # conv batch act
+        x = self.conv(input)
+        x = self.bn(x)
+        x = self.gelu(x)
         
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.gelu(x)
         
-        
-    def forward(self, x):
+        x = x + input
         
         return x
 
@@ -19,17 +36,23 @@ class ResNet(nn.Module):
     def __init__(self):
         super(ResNet, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
-        self.conv = CNN_block(1, 64, 3, 2)
         
     def forward(self, x):
+        # 28
+        # conv1
         
-        x = self.conv1(x)
-        x = self.maxpool(x)
-        print(x.shape)
+
+        # 14
+        # cov2
         
+        # 7
+        # conv3
+        
+        # 3 
+        # avgpool        
+
+       
         return x
 
 
